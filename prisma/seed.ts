01b0fed184eb030eb151d0590d470de5cfcdf448
kautilya2000 <kankipatikautilya1@gmail.com>
2023-03-13 13:09:57 -0400

    init commit

---
+++ prisma/seed.ts
@@ -0,0 +1,29 @@
+import { PrismaClient } from '@prisma/client'
+import students from './students'
+
+const prisma = new PrismaClient()
+
+async function main() {
+    for (const student of students) {
+        await prisma.student.create({
+            data: {
+                email: student.email,
+                firstName: student.firstName,
+                lastName: student.lastName,
+                homeDepartment: student.homeDepartment,
+                ksuId: student.id,
+                phoneNumber: student.phoneNumber,
+            },
+        })
+    }
+}
+
+main()
+  .then(async () => {
+    await prisma.$disconnect()
+  })
+  .catch(async (e) => {
+    console.error(e)
+    await prisma.$disconnect()
+    process.exit(1)
+  })
